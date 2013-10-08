7c2d2336b153b18e949d63c90f9f6da38544aaa7
gitnsy <jump24d@gmai.com>
2013-10-08 21:30:42 +0900

    foo

---
+++ BB_OfflineAsm/Scripts/asmsim/data/setbonus.js
@@ -0,0 +1,9 @@
+﻿asmsim.data = asmsim.data || {};
+(function (data) {
+    function SetBonus(bland, description) {
+        this.bland = bland;
+        this.description = description;
+    }
+
+    data.setbonus = [new SetBonus("Cooger" , "重量耐性UP(150)")];
+})(asmsim.data);
