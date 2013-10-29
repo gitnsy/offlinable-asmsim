2c31b84c05d2897f2e543feb6571e644f2d28d7f
gitnsy <jump24d@gmai.com>
2013-10-30 07:06:40 +0900

    未使用ライブライの削除、シリアライズ機能の追加、テストのつくりはじめとか

--- BB_OfflineAsm/test/spec/ViewModelSpec.js
+++ BB_OfflineAsm/test/spec/ViewModelSpec.js
@@ -1,2 +1,297 @@
-﻿/// <reference path="../jasmine.js" />
+﻿/// <reference path="../lib/jasmine-1.3.0/jasmine.js" />
 /// <reference path="/Scripts/asmsim/ViewModel.js" />
+
+describe("ViewModelテスト", function () {
+    it("デシリアライズ", function () {
+        expect().toEqual(false);
+    });
+
+    //xdescribeで一時的に無効化
+    xdescribe("ViewModel各プロパティテスト", function () {
+        var vm = new asmsim.viewModel({});
+
+        describe("observable", function () {
+            it("filterArmWeight()", function () {
+                expect(vm.filterArmWeight()).toEqual(0);
+            });
+
+            it("filterBodyWeight()", function () {
+                expect(vm.filterBodyWeight()).toEqual(0);
+            });
+
+            it("filterHeadWeight()", function () {
+                expect(vm.filterHeadWeight()).toEqual(0);
+            });
+
+            it("filterLegWeight()", function () {
+                expect(vm.filterLegWeight()).toEqual(0);
+            });
+
+            it("exParams()", function () {
+                expect(vm.exParams()).toEqual();
+            });
+
+            it("selectedAssaultAssist()", function () {
+                expect(vm.selectedAssaultAssist()).toEqual();
+            });
+
+            it("selectedAssaultMain()", function () {
+                expect(vm.selectedAssaultMain()).toEqual();
+            });
+
+            it("selectedAssaultSp()", function () {
+                expect(vm.selectedAssaultSp()).toEqual();
+            });
+
+            it("selectedAssaultSub()", function () {
+                expect(vm.selectedAssaultSub()).toEqual();
+            });
+
+            it("selectedHeavyAssist()", function () {
+                expect(vm.selectedHeavyAssist()).toEqual();
+            });
+
+            it("selectedHeavyMain()", function () {
+                expect(vm.selectedHeavyMain()).toEqual();
+            });
+
+            it("selectedHeavySp()", function () {
+                expect(vm.selectedHeavySp()).toEqual();
+            });
+
+            it("selectedHeavySub()", function () {
+                expect(vm.selectedHeavySub()).toEqual();
+            });
+
+            it("selectedSniperAssist()", function () {
+                expect(vm.selectedSniperAssist()).toEqual();
+            });
+
+            it("selectedSniperMain()", function () {
+                expect(vm.selectedSniperMain()).toEqual();
+            });
+
+            it("selectedSniperSp()", function () {
+                expect(vm.selectedSniperSp()).toEqual();
+            });
+
+            it("selectedSniperSub()", function () {
+                expect(vm.selectedSniperSub()).toEqual();
+            });
+
+            it("selectedSupportAssist()", function () {
+                expect(vm.selectedSupportAssist()).toEqual();
+            });
+
+            it("selectedSupportMain()", function () {
+                expect(vm.selectedSupportMain()).toEqual();
+            });
+
+            it("selectedSupportSp()", function () {
+                expect(vm.selectedSupportSp()).toEqual();
+            });
+
+            it("selectedSupportSub()", function () {
+                expect(vm.selectedSupportSub()).toEqual();
+            });
+
+            it("addBoltonArmWeight()", function () {
+                expect(vm.addBoltonArmWeight()).toEqual();
+            });
+
+            it("selectedArm()", function () {
+                expect(vm.selectedArm()).toEqual();
+            });
+
+            it("selectedBody()", function () {
+                expect(vm.selectedBody()).toEqual();
+            });
+
+            it("selectedBoltOnArm()", function () {
+                expect(vm.selectedBoltOnArm()).toEqual();
+            });
+
+            it("selectedHead()", function () {
+                expect(vm.selectedHead()).toEqual();
+            });
+
+            it("selectedLeg()", function () {
+                expect(vm.selectedLeg()).toEqual();
+            });
+
+        });
+        describe("observableArray", function () {
+            it("armFilters()", function () {
+                expect(vm.armFilters()).toEqual();
+            });
+
+            it("bodyFilters()", function () {
+                expect(vm.bodyFilters()).toEqual();
+            });
+
+            it("headFilters()", function () {
+                expect(vm.headFilters()).toEqual();
+            });
+
+            it("legFilters()", function () {
+                expect(vm.legFilters()).toEqual();
+            });
+
+            it("actionChipList()", function () {
+                expect(vm.actionChipList()).toEqual();
+            });
+
+            it("advanceChipList()", function () {
+                expect(vm.advanceChipList()).toEqual();
+            });
+
+            it("enhanceChipList()", function () {
+                expect(vm.enhanceChipList()).toEqual();
+            });
+
+            it("armList()", function () {
+                expect(vm.armList()).toEqual();
+            });
+
+            it("assaultAssistWeapons()", function () {
+                expect(vm.assaultAssistWeapons()).toEqual();
+            });
+
+            it("assaultMainWeapons()", function () {
+                expect(vm.assaultMainWeapons()).toEqual();
+            });
+
+            it("assaultSpWeapons()", function () {
+                expect(vm.assaultSpWeapons()).toEqual();
+            });
+
+            it("assaultSubWeapons()", function () {
+                expect(vm.assaultSubWeapons()).toEqual();
+            });
+
+            it("bodyList()", function () {
+                expect(vm.bodyList()).toEqual();
+            });
+
+            it("headList()", function () {
+                expect(vm.headList()).toEqual();
+            });
+
+            it("heavyAssistWeapons()", function () {
+                expect(vm.heavyAssistWeapons()).toEqual();
+            });
+
+            it("heavyMainWeapons()", function () {
+                expect(vm.heavyMainWeapons()).toEqual();
+            });
+
+            it("heavySpWeapons()", function () {
+                expect(vm.heavySpWeapons()).toEqual();
+            });
+
+            it("heavySubWeapons()", function () {
+                expect(vm.heavySubWeapons()).toEqual();
+            });
+
+            it("legList()", function () {
+                expect(vm.legList()).toEqual();
+            });
+
+            it("sniperAssistWeapons()", function () {
+                expect(vm.sniperAssistWeapons()).toEqual();
+            });
+
+            it("sniperMainWeapons()", function () {
+                expect(vm.sniperMainWeapons()).toEqual();
+            });
+
+            it("sniperSpWeapons()", function () {
+                expect(vm.sniperSpWeapons()).toEqual();
+            });
+
+            it("sniperSubWeapons()", function () {
+                expect(vm.sniperSubWeapons()).toEqual();
+            });
+
+            it("supportAssistWeapons()", function () {
+                expect(vm.supportAssistWeapons()).toEqual();
+            });
+
+            it("supportMainWeapons()", function () {
+                expect(vm.supportMainWeapons()).toEqual();
+            });
+
+            it("supportSpWeapons()", function () {
+                expect(vm.supportSpWeapons()).toEqual();
+            });
+
+            it("supportSubWeapons()", function () {
+                expect(vm.supportSubWeapons()).toEqual();
+            });
+
+            it("boltonArmList()", function () {
+                expect(vm.boltonArmList()).toEqual();
+            });
+
+        });
+        describe("computed", function () {
+            it("armorAvarage()", function () {
+                expect(vm.armorAvarage()).toEqual();
+            });
+
+            it("assaultWeight()", function () {
+                expect(vm.assaultWeight()).toEqual();
+            });
+
+            it("capacity()", function () {
+                expect(vm.capacity()).toEqual();
+            });
+
+            it("filterdArm()", function () {
+                expect(vm.filterdArm()).toEqual();
+            });
+
+            it("filterdBody()", function () {
+                expect(vm.filterdBody()).toEqual();
+            });
+
+            it("filterdHead()", function () {
+                expect(vm.filterdHead()).toEqual();
+            });
+
+            it("filterdLeg()", function () {
+                expect(vm.filterdLeg()).toEqual();
+            });
+
+            it("heavyWeight()", function () {
+                expect(vm.heavyWeight()).toEqual();
+            });
+
+            it("setBonus()", function () {
+                expect(vm.setBonus()).toEqual();
+            });
+
+            it("sniperWeight()", function () {
+                expect(vm.sniperWeight()).toEqual();
+            });
+
+            it("supportWeight()", function () {
+                expect(vm.supportWeight()).toEqual();
+            });
+
+            it("totalChipCapacity()", function () {
+                expect(vm.totalChipCapacity()).toEqual();
+            });
+
+            it("totalChipCost()", function () {
+                expect(vm.totalChipCost()).toEqual();
+            });
+
+            it("totalWeight()", function () {
+                expect(vm.totalWeight()).toEqual();
+            });
+
+        });
+    });
+
+});
---
+++ BB_OfflineAsm/test/spec/serializerSpec.js
@@ -0,0 +1,67 @@
+﻿/// <reference path="../lib/jasmine-1.3.0/jasmine.js" />
+/// <reference path="/Scripts/asmsim/ViewModel.js" />
+/// <reference path="/Scripts/asmsim/serializer.js" />
+
+describe("シリアライザテスト", function () {
+    //テストスタブ
+    var s = {
+        selectedHead: function () { return { bland: "head", rank: 1 }; },
+        selectedBody: function () { return { bland: "body", rank: 2 }; },
+        selectedArm: function () { return { bland: "arm", rank: 3 }; },
+        selectedLeg: function () { return { bland: "leg", rank: 4 }; },
+        selectedAssaultMain: function () { return { bland: "AsMain", rank: 5 }; },
+        selectedAssaultSub: function () { return { bland: "AsSub", rank: 6 }; },
+        selectedAssaultAssist: function () { return { bland: "AsAssist", rank: 7 }; },
+        selectedAssaultSp: function () { return { bland: "AsSp", rank: 8 }; },
+        selectedHeavyMain: function () { return { bland: "HeMain", rank: 9 }; },
+        selectedHeavySub: function () { return { bland: "HeSub", rank: 10 }; },
+        selectedHeavyAssist: function () { return { bland: "HeAssist", rank: 11 }; },
+        selectedHeavySp: function () { return { bland: "HeSp", rank: 12 }; },
+        selectedSupportMain: function () { return { bland: "SuMain", rank: 13 }; },
+        selectedSupportSub: function () { return { bland: "SuSub", rank: 14 }; },
+        selectedSupportAssist: function () { return { bland: "SuAssist", rank: 15 }; },
+        selectedSupportSp: function () { return { bland: "SuSp", rank: 16 }; },
+        selectedSniperMain: function () { return { bland: "SnMain", rank: 17 }; },
+        selectedSniperSub: function () { return { bland: "SnSub", rank: 18 }; },
+        selectedSniperAssist: function () { return { bland: "SnAssist", rank: 19 }; },
+        selectedSniperSp: function () { return { bland: "SnSp", rank: 20 }; },
+        enhanceChipList: function () {
+            return [
+                { selected: function () { return false; }, bland: "bad", rank: -1 },
+                { selected: function () { return true; }, bland: "ec", rank: 21 },
+                { selected: function () { return true; }, bland: "ec", rank: 22 },
+                { selected: function () { return false; }, bland: "bad", rank: -2 }
+            ];
+        },
+        actionChipList: function () {
+            return [
+                { selected: function () { return false; }, bland: "bad", rank: -1 },
+                { selected: function () { return true; }, bland: "ac", rank: 23 },
+                { selected: function () { return true; }, bland: "ac", rank: 24 },
+                { selected: function () { return false; }, bland: "bad", rank: -2 }
+            ];
+        },
+        advanceChipList: function () {
+            return [
+                { selected: function () { return false; }, bland: "bad", rank: -1 },
+                { selected: function () { return true; }, bland: "ad", rank: 25 },
+                { selected: function () { return true; }, bland: "ad", rank: 26 },
+                { selected: function () { return false; }, bland: "bad", rank: -2 }
+            ];
+        }
+    };
+
+    it("ViewModelの情報をURL用の情報に変換", function () {
+        //順番はどうでもいい仕様にするからテストそこ考慮してもいいかも
+        expect(asmsim.serializer.toUrl(s))
+            .toEqual("h:head_1,b:body_2,a:arm_3,l:leg_4,"
+            + "am:AsMain_5,as:AsSub_6,aa:AsAssist_7,ax:AsSp_8,"
+            + "hm:HeMain_9,hs:HeSub_10,ha:HeAssist_11,hx:HeSp_12,"
+            + "sm:SuMain_13,ss:SuSub_14,sa:SuAssist_15,sx:SuSp_16,"
+            + "nm:SnMain_17,ns:SnSub_18,na:SnAssist_19,nx:SnSp_20,"
+            + "ce:ec_21.ec_22,ca:ac_23.ac_24,cm:ad_25.ad_26");
+    });
+    it("URL用の情報にViewModelに変換", function () {
+        vm.apply({ a: { bland: "Cooger", rank: 2 } });
+    });
+});
