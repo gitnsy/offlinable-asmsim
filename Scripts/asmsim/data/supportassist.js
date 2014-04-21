e2570c6c4ce0d85a3ba3e323f26cc8db15b227f4
gitnsy <jump24d@gmail.com>
2014-04-21 23:16:57 +0900

    4.0

--- Scripts/asmsim/data/supportassist.js
+++ Scripts/asmsim/data/supportassist.js
@@ -7,14 +7,14 @@
         this.rank = rank;
     }
         data.supportassist = [new SupportAssist("ラーク偵察機", "spyplane", 400,1)
-, new SupportAssist("ファルコン偵察機", "spyplane", 420,2)
-, new SupportAssist("アウル偵察機", "spyplane", 460,3)
+, new SupportAssist("ファルコン偵察機", "spyplane", 390,2)
+, new SupportAssist("アウル偵察機", "spyplane", 420,3)
 , new SupportAssist("ロビン偵察機", "spyplane", 410,4)
 , new SupportAssist("ストーク偵察機", "spyplane", 430,5)
-, new SupportAssist("ガルーダ", "spyplane", 420,2.1)
+, new SupportAssist("ガルーダ", "spyplane", 390,2.1)
 , new SupportAssist("索敵センサー", "sensor", 350,1)
 , new SupportAssist("小型索敵センサー", "sensor", 280,2)
-, new SupportAssist("広域索敵センサー", "sensor", 400,3)
+, new SupportAssist("広域索敵センサー", "sensor", 380,3)
 , new SupportAssist("軽量索敵センサー", "sensor", 250,4)
 , new SupportAssist("滞空索敵弾", "airsensor", 370,1)
 , new SupportAssist("小型滞空索敵弾", "airsensor", 360, 2)
